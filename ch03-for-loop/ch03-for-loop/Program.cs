24f426c4a29104c2d252cc8d21e3b717e919c1cb
Derek Lee <dlee133@gmail.com>
2020-12-02 18:28:11 -0500

    for-loop

---
+++ ch03-for-loop/ch03-for-loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ch03_for_loop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("for loops!");
+			// indexed for loo[ that will print "hello" n times
+			// n is an int entered by the user
+
+			Console.WriteLine("Enter a whole number: ");
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine("Hello! " + i);
+				{
+
+				}
+
+
+			}
+		}
+	}
+}
+
---
+++ ch03-switch-statement/ch03-switch-statement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ch03_switch_statement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("switch statement demo");
+			// prompt usert for a #between 1 and 7
+			// convert that # to a day of the week
+			Console.Write("Enter a number between 1 and 7: ");
+			int n = Int32.Parse(Console.ReadLine());
+			String dow = "";
+			switch (n)
+			{
+				case 1:
+					dow = "Monday";
+					break;
+				case 2:
+					dow = "Tuesday";
+					break;
+				case 3:
+					dow = "Wednesday";
+					break;
+				case 4:
+					dow = "Thursday";
+					break;
+				case 5:
+					dow = "Friday";
+					break;
+				case 6:
+					dow = "Saturday";
+					break;
+				case 7:
+					dow = "Sunday";
+					break;
+				default:
+					dow = "invalid dow";
+					break;
+			}
+			Console.WriteLine("Day of the week is '" + dow + "'.");
+
+
+			Console.WriteLine("Bye!");
+		}
+	}
+}
