64a9757c724fb21b755dd15c87bd4b53430e9da1
Derek Lee <dlee133@gmail.com>
2020-12-02 17:20:44 -0500

    time-travel-cal

---
+++ ch03-pro02-travel-time-calculator/ch03-pro02-travel-time-calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ch03_pro02_travel_time_calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the Travel Time Calculator!");
+			String choice = "y";
+			while (choice.Equals("y"))
+			{
+				Console.Write("Enter Miles: ");
+				String dist = Console.ReadLine();
+				Console.Write("Enter Miles Per Hour: ");
+				String mph = Console.ReadLine();
+				Double dist1 = Convert.ToDouble(dist);
+				Double mph1 = Convert.ToDouble(mph);
+
+			}
+			Console.WriteLine("Bye!");
+		}
+	}
+}
