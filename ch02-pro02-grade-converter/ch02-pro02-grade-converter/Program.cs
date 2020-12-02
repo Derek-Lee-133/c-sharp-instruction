e073c0f0c68f242eb30c2f8f32b3b27f503fcde9
Derek Lee <dlee133@gmail.com>
2020-12-02 14:31:37 -0500

    cho2-pro02 finish

--- ch02-pro02-grade-converter/ch02-pro02-grade-converter/Program.cs
+++ ch02-pro02-grade-converter/ch02-pro02-grade-converter/Program.cs
@@ -12,13 +12,44 @@ namespace ch02_pro02_grade_converter
 			while (choice.Equals("y"))
 			{
 				Console.Write("Enter numerical grade: ");
-					String g = Console.ReadLine(); {
-					
-					if (g >= 88)
+				String g = Console.ReadLine();
+				double g1 = Convert.ToDouble(g);
+				{
 
-}
+					if (g1 >= 88)
+					{
+						Console.WriteLine("A");
+
+					}
+					else if (g1 >= 80)
+					{
+						Console.WriteLine("B");
+
+
+					}
+					else if (g1 >= 67)
+					{
+						Console.WriteLine("C");
+					}
+					else if (g1 >= 60)
+					{
+						Console.WriteLine("D");
+					}
+					else
+					{
+						Console.WriteLine("F");
+					}
+					Console.WriteLine("Continue? (y/n): ");
+					choice= Console.ReadLine();
+
+				}
+				Console.WriteLine("Bye");
+				
 
 
 			}
+			
+		}
+		
 	}
 }
